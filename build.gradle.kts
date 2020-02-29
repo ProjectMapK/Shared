84fda055b49f9de2fe2c667d0c41fbb200e26def
wrongwrong <boranti1995@gmail.com>
2020-03-01 01:52:46 +0900

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,57 @@
+plugins {
+    id("maven")
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.3.61"
+    id("org.jlleitschuh.gradle.ktlint") version "9.2.1"
+}
+
+group = "com.mapk"
+version = "0.1"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+
+buildscript {
+    repositories {
+        mavenCentral()
+    }
+
+    dependencies {
+        classpath(kotlin("gradle-plugin", version = "1.3.61"))
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation(kotlin("reflect"))
+
+    // https://mvnrepository.com/artifact/org.junit.jupiter/junit-jupiter
+    testImplementation(group = "org.junit.jupiter", name = "junit-jupiter", version = "5.6.0") {
+        exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+    }
+}
+
+tasks {
+    compileKotlin {
+        dependsOn("ktlintFormat")
+        kotlinOptions {
+            jvmTarget = "1.8"
+            allWarningsAsErrors = true
+        }
+    }
+
+    compileTestKotlin {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+    test {
+        useJUnitPlatform()
+    }
+}
