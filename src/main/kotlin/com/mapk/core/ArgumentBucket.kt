1963531c607eeb3797487a7e7b3127d1b443dc2f
wrongwrong <boranti1995@gmail.com>
2020-03-08 22:18:16 +0900

    マップの生成方法を修正

--- src/main/kotlin/com/mapk/core/ArgumentBucket.kt
+++ src/main/kotlin/com/mapk/core/ArgumentBucket.kt
@@ -4,19 +4,25 @@ import kotlin.reflect.KParameter
 
 internal class BucketGenerator(
     private val bucket: Array<Any?>,
-    private val bucketMap: MutableMap<KParameter, Any?>,
+    private val instancePair: Pair<KParameter, Any?>?,
     private val initializationStatus: Int,
     private val initializeMask: List<Int>
 ) {
     private val completionValue: Int = initializeMask.reduce { l, r -> l or r }
+    private val bucketSize = bucket.size
 
-    fun generate(): ArgumentBucket = ArgumentBucket(
-        bucket.copyOf(),
-        bucketMap.toMutableMap(),
-        initializationStatus,
-        initializeMask,
-        completionValue
-    )
+    fun generate(): ArgumentBucket {
+        val tempMap = HashMap<KParameter, Any?>(bucketSize, 1.0f)
+        instancePair?.run { tempMap[this.first] = this.second }
+
+        return ArgumentBucket(
+            bucket.copyOf(),
+            tempMap,
+            initializationStatus,
+            initializeMask,
+            completionValue
+        )
+    }
 }
 
 class ArgumentBucket internal constructor(
