f197914fd9e7e6aae29ff97abf6fb052d3f7fa42
wrongwrong <boranti1995@gmail.com>
2020-03-09 00:31:22 +0900

    修正に合わせて対応

--- src/main/kotlin/com/mapk/core/KFunctionForCall.kt
+++ src/main/kotlin/com/mapk/core/KFunctionForCall.kt
@@ -16,19 +16,16 @@ class KFunctionForCall<T>(private val function: KFunction<T>, instance: Any? = n
         function.isAccessible = true
 
         // 初期化処理の共通化のため先に初期化
-        val tempMap = HashMap<KParameter, Any?>(parameters.size, 1.0f)
         val tempArray = Array<Any?>(parameters.size) { null }
         val maskList = generateSequence(1) { it.shl(1) }.take(parameters.size).toList()
 
         generator = if (instance != null) {
-            // インスタンス有りでは先にインスタンスを初期化する
-            parameters.find { it.kind == KParameter.Kind.INSTANCE }?.run { tempMap[this] = instance }
             tempArray[0] = instance
 
             // 引数の1番目は初期化済みということでinitializationStatusは1スタート
-            BucketGenerator(tempArray, tempMap, 1, maskList)
+            BucketGenerator(tempArray, parameters.first { it.kind == KParameter.Kind.INSTANCE } to instance, 1, maskList)
         } else {
-            BucketGenerator(tempArray, tempMap, 0, maskList)
+            BucketGenerator(tempArray, null, 0, maskList)
         }
     }
 
