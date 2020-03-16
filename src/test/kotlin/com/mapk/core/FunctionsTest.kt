8131ccfd58bedca7af0521c586b332cd8adac04c
wrongwrong <boranti1995@gmail.com>
2020-03-17 07:30:03 +0900

    エイリアスを貼った時のテストを追加

---
+++ src/test/kotlin/com/mapk/core/FunctionsTest.kt
@@ -0,0 +1,42 @@
+package com.mapk.core
+
+import com.mapk.annotations.KParameterAlias
+import io.mockk.every
+import io.mockk.mockk
+import kotlin.reflect.KParameter
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("関数類のテスト")
+class FunctionsTest {
+    @Nested
+    @DisplayName("エイリアス関連のテスト")
+    inner class GetAliasOrNameTest {
+        @Test
+        @DisplayName("エイリアス無し")
+        fun noAlias() {
+            val paramName = "mocked name"
+            val parameter = mockk<KParameter>()
+            every { parameter.annotations } returns emptyList()
+            every { parameter.name } returns paramName
+
+            assertEquals(paramName, parameter.getAliasOrName())
+        }
+
+        @Test
+        @DisplayName("エイリアス有り")
+        fun withAlias() {
+            val aliasedName = "aliased name"
+
+            val alias = mockk<KParameterAlias>()
+            every { alias.value } returns aliasedName
+
+            val parameter = mockk<KParameter>()
+            every { parameter.annotations } returns listOf(alias)
+
+            assertEquals(aliasedName, parameter.getAliasOrName())
+        }
+    }
+}
