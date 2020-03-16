a43b435a1c4fd53fa3311086743eda731215226d
wrongwrong <boranti1995@gmail.com>
2020-03-17 07:34:26 +0900

    beforeEachで簡略化

--- src/test/kotlin/com/mapk/core/FunctionsTest.kt
+++ src/test/kotlin/com/mapk/core/FunctionsTest.kt
@@ -5,12 +5,20 @@ import io.mockk.every
 import io.mockk.mockk
 import kotlin.reflect.KParameter
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 
 @DisplayName("関数類のテスト")
 class FunctionsTest {
+    lateinit var parameter: KParameter
+
+    @BeforeEach
+    fun beforeEach() {
+        parameter = mockk()
+    }
+
     @Nested
     @DisplayName("エイリアス関連のテスト")
     inner class GetAliasOrNameTest {
@@ -18,7 +26,6 @@ class FunctionsTest {
         @DisplayName("エイリアス無し")
         fun noAlias() {
             val paramName = "mocked name"
-            val parameter = mockk<KParameter>()
             every { parameter.annotations } returns emptyList()
             every { parameter.name } returns paramName
 
@@ -33,7 +40,6 @@ class FunctionsTest {
             val alias = mockk<KParameterAlias>()
             every { alias.value } returns aliasedName
 
-            val parameter = mockk<KParameter>()
             every { parameter.annotations } returns listOf(alias)
 
             assertEquals(aliasedName, parameter.getAliasOrName())
