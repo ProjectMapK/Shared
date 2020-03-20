b3798a4a784717961b13cde70a441cdd0eeaef15
wrongwrong <boranti1995@gmail.com>
2020-03-20 14:38:28 +0900

    テストを1段ネスト

--- src/test/kotlin/com/mapk/core/ArgumentBucketTest.kt
+++ src/test/kotlin/com/mapk/core/ArgumentBucketTest.kt
@@ -16,52 +16,56 @@ private fun sampleFunction(arg1: Any?, arg2: Any?, arg3: Any?) {
 
 @DisplayName("ArgumentBucketTestのテスト")
 class ArgumentBucketTest {
-    private lateinit var argumentBucket: ArgumentBucket
-
-    @BeforeEach
-    fun beforeEach() {
-        argumentBucket = KFunctionForCall(::sampleFunction).getArgumentBucket()
-    }
-
     @Nested
-    @DisplayName("初期化状態のチェックテスト")
-    inner class IsInitializedTest {
-        @Test
-        @DisplayName("初期化前")
-        fun isNotInitialized() {
-            assertFalse(argumentBucket.isInitialized)
+    @DisplayName("シンプルな呼び出しのテスト")
+    inner class SimpleTest {
+        private lateinit var argumentBucket: ArgumentBucket
+
+        @BeforeEach
+        fun beforeEach() {
+            argumentBucket = KFunctionForCall(::sampleFunction).getArgumentBucket()
         }
 
-        @Test
-        @DisplayName("初期化後")
-        fun isInitialized() {
-            ::sampleFunction.parameters.forEach {
-                argumentBucket.putIfAbsent(it, object {})
+        @Nested
+        @DisplayName("初期化状態のチェックテスト")
+        inner class IsInitializedTest {
+            @Test
+            @DisplayName("初期化前")
+            fun isNotInitialized() {
+                assertFalse(argumentBucket.isInitialized)
             }
 
-            assertTrue(argumentBucket.isInitialized)
-        }
-    }
+            @Test
+            @DisplayName("初期化後")
+            fun isInitialized() {
+                ::sampleFunction.parameters.forEach {
+                    argumentBucket.putIfAbsent(it, object {})
+                }
 
-    @Nested
-    @DisplayName("引数セットのテスト")
-    inner class SetArgumentTest {
-        @Test
-        @DisplayName("正常に追加した場合")
-        fun setNewArgument() {
-            val parameter = ::sampleFunction.parameters.first { it.index == 0 }
-            argumentBucket.putIfAbsent(parameter, "argument")
-            assertEquals("argument", argumentBucket.getByIndex(0))
+                assertTrue(argumentBucket.isInitialized)
+            }
         }
 
-        @Test
-        @DisplayName("同じインデックスに2回追加した場合")
-        fun setArgumentTwice() {
-            val parameter = ::sampleFunction.parameters.first { it.index == 0 }
+        @Nested
+        @DisplayName("引数セットのテスト")
+        inner class SetArgumentTest {
+            @Test
+            @DisplayName("正常に追加した場合")
+            fun setNewArgument() {
+                val parameter = ::sampleFunction.parameters.first { it.index == 0 }
+                argumentBucket.putIfAbsent(parameter, "argument")
+                assertEquals("argument", argumentBucket.getByIndex(0))
+            }
 
-            argumentBucket.putIfAbsent(parameter, "first")
-            argumentBucket.putIfAbsent(parameter, "second")
-            assertEquals("first", argumentBucket.getByIndex(0))
+            @Test
+            @DisplayName("同じインデックスに2回追加した場合")
+            fun setArgumentTwice() {
+                val parameter = ::sampleFunction.parameters.first { it.index == 0 }
+
+                argumentBucket.putIfAbsent(parameter, "first")
+                argumentBucket.putIfAbsent(parameter, "second")
+                assertEquals("first", argumentBucket.getByIndex(0))
+            }
         }
     }
 }
