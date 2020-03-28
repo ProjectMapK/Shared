306b6d959d4ed2b6400a4d21a314922a88a71d1f
wrongwrong <boranti1995@gmail.com>
2020-03-29 01:12:26 +0900

    生成関数を全面的に書き直し

--- src/main/java/com/mapk/core/BucketGenerator.java
+++ src/main/java/com/mapk/core/BucketGenerator.java
@@ -10,10 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class BucketGenerator {
-    private final int initializationStatus;
-    @NotNull
-    private final List<Long> initializeMask;
-    private final long completionValue;
+    private final Boolean[] initializationStatus;
 
     @NotNull
     private final List<Boolean> isRequireNonNull;
@@ -25,33 +22,27 @@ class BucketGenerator {
     BucketGenerator(List<KParameter> parameters, @Nullable Pair<KParameter, Object> instancePair) {
         final int capacity = parameters.size();
 
-        keyArray = new KParameter[capacity];
-        valueArray = new Object[capacity];
-
-        if (instancePair != null) {
-            initializationStatus = 1;
-
-            keyArray[0] = instancePair.getFirst();
-            valueArray[0] = instancePair.getSecond();
-        } else {
-            initializationStatus = 0;
-        }
-
         isRequireNonNull = new ArrayList<>(capacity);
-        initializeMask = new ArrayList<>(capacity);
-        int completionValue = 0;
+        initializationStatus = new Boolean[capacity];
 
-        long mask = 1;
-        for (int i = 0; i < capacity; i++, mask <<= 1) {
+        for (int i = 0; i < capacity; i++) {
             isRequireNonNull.add(
                     i,
                     parameters.get(i).getAnnotations().stream().anyMatch(it -> it instanceof KParameterRequireNonNull)
             );
-            initializeMask.add(i, mask);
-            completionValue |= mask;
+            initializationStatus[i] = false;
         }
 
-        this.completionValue = completionValue;
+        keyArray = new KParameter[capacity];
+        valueArray = new Object[capacity];
+
+        if (instancePair != null) {
+            keyArray[0] = instancePair.getFirst();
+            valueArray[0] = instancePair.getSecond();
+            initializationStatus[0] = true;
+        } else {
+            initializationStatus[0] = false;
+        }
     }
 
     @NotNull
@@ -60,7 +51,7 @@ class BucketGenerator {
                 keyArray.clone(),
                 valueArray.clone(),
                 isRequireNonNull,
-                new BitFlagInitializationStatusManager(initializationStatus, initializeMask, completionValue)
+                new InitializationStatusManagerImpl(initializationStatus.clone())
         );
     }
 }
