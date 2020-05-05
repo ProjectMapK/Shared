8c2555480b1ebe9b44777f259ea5ff6275b5862b
wrongwrong <boranti1995@gmail.com>
2020-05-06 07:17:40 +0900

    abc順に並べ替え

--- src/main/kotlin/com/mapk/core/ValueParameter.kt
+++ src/main/kotlin/com/mapk/core/ValueParameter.kt
@@ -4,28 +4,28 @@ import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 
 interface ValueParameter<T : Any> {
-    val requiredClazz: KClass<T>
-    val name: String
     val annotations: List<Annotation>
     val isOptional: Boolean
+    val name: String
+    val requiredClazz: KClass<T>
 }
 
 // TODO: 仮置き、ArgumentBinder.Valueでimplementした方が良いと感じるが一旦実装を分けている
 internal class ValueParameterImpl<T : Any>(
-    override val requiredClazz: KClass<T>,
-    override val name: String,
     override val annotations: List<Annotation>,
-    override val isOptional: Boolean
+    override val isOptional: Boolean,
+    override val name: String,
+    override val requiredClazz: KClass<T>
 ) : ValueParameter<T> {
     companion object {
         fun newInstance(
             parameter: KParameter,
             parameterNameConverter: (String) -> String
         ): ValueParameter<*> = ValueParameterImpl(
-            parameter.type.classifier as KClass<*>,
-            parameterNameConverter(parameter.getAliasOrName()!!),
             parameter.annotations,
-            parameter.isOptional
+            parameter.isOptional,
+            parameterNameConverter(parameter.getAliasOrName()!!),
+            parameter.type.classifier as KClass<*>
         )
     }
 }
