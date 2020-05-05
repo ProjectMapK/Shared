dc822b4e11545d27cc575e821ef119a0230289e1
wrongwrong <boranti1995@gmail.com>
2020-05-06 07:20:44 +0900

    nullabilityを追加

--- src/main/kotlin/com/mapk/core/ValueParameter.kt
+++ src/main/kotlin/com/mapk/core/ValueParameter.kt
@@ -1,10 +1,12 @@
 package com.mapk.core
 
+import com.mapk.annotations.KParameterRequireNonNull
 import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
 
 interface ValueParameter<T : Any> {
     val annotations: List<Annotation>
+    val isNullable: Boolean
     val isOptional: Boolean
     val name: String
     val requiredClazz: KClass<T>
@@ -13,6 +15,7 @@ interface ValueParameter<T : Any> {
 // TODO: 仮置き、ArgumentBinder.Valueでimplementした方が良いと感じるが一旦実装を分けている
 internal class ValueParameterImpl<T : Any>(
     override val annotations: List<Annotation>,
+    override val isNullable: Boolean,
     override val isOptional: Boolean,
     override val name: String,
     override val requiredClazz: KClass<T>
@@ -23,6 +26,7 @@ internal class ValueParameterImpl<T : Any>(
             parameterNameConverter: (String) -> String
         ): ValueParameter<*> = ValueParameterImpl(
             parameter.annotations,
+            parameter.annotations.any { it is KParameterRequireNonNull },
             parameter.isOptional,
             parameterNameConverter(parameter.getAliasOrName()!!),
             parameter.type.classifier as KClass<*>
