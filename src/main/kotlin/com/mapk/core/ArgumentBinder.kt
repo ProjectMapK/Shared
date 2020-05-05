382cefc9258ef1bc502846675f74b52258f5c072
wrongwrong <boranti1995@gmail.com>
2020-05-06 06:08:01 +0900

    値のバインド方法を表すクラスを追加

---
+++ src/main/kotlin/com/mapk/core/ArgumentBinder.kt
@@ -0,0 +1,23 @@
+package com.mapk.core
+
+// TODO: 関数バインダーの追加
+sealed class ArgumentBinder {
+    abstract val index: Int
+
+    /**
+     * 初期化されており、かつ読み出し条件に合致すれば読み出してその値をバインド、されていなければ何もしない
+     * @return バインドしたかどうか
+     */
+    abstract fun bindArgument(adaptor: ArgumentAdaptor, valueArray: Array<Any?>): Boolean
+
+    class Value(private val key: String, override val index: Int) : ArgumentBinder() {
+        override fun bindArgument(adaptor: ArgumentAdaptor, valueArray: Array<Any?>): Boolean {
+            return if (adaptor.isInitialized(key)) {
+                valueArray[index] = adaptor.readout(key)
+                true
+            } else {
+                false
+            }
+        }
+    }
+}
