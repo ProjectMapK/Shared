712fa6952a815df6cb41d1f58d961cfc2b4063a5
wrongwrong <boranti1995@gmail.com>
2020-05-06 07:45:00 +0900

    フラット化が必要な場合の分岐を追加

--- src/main/kotlin/com/mapk/core/BucketGenerator.kt
+++ src/main/kotlin/com/mapk/core/BucketGenerator.kt
@@ -1,6 +1,9 @@
 package com.mapk.core
 
+import com.mapk.annotations.KParameterFlatten
+import kotlin.reflect.KClass
 import kotlin.reflect.KParameter
+import kotlin.reflect.full.findAnnotation
 
 internal class BucketGenerator(
     private val parameters: List<KParameter>,
@@ -13,8 +16,24 @@ internal class BucketGenerator(
     private val originalInitializationStatus: Array<Boolean>
 
     init {
-        // TODO: ネスト関連への対応
-        binders = filteredParameters.map { ArgumentBinder.Value(parameterNameConverter(it.getAliasOrName()!!), it.index) }
+        binders = filteredParameters.map {
+            val name = it.getAliasOrName()!!
+
+            it.findAnnotation<KParameterFlatten>()?.let { annotation ->
+                // 名前の変換処理、結合が必要な場合はインスタンスを持ってきて対応する
+                val converter: (String) -> String = if (annotation.fieldNameToPrefix) {
+                    val joiner = annotation.namingConvention.objectInstance!!
+                    { suffix -> parameterNameConverter(joiner.join(name, suffix)) }
+                } else {
+                    parameterNameConverter
+                }
+
+                ArgumentBinder.Function(
+                    (it.type.classifier as KClass<*>).toKConstructor(converter),
+                    it.index
+                )
+            } ?: ArgumentBinder.Value(parameterNameConverter(name), it.index)
+        }
         // 配列系ではフィルタリング前のサイズが必要
         originalValueArray = arrayOfNulls(parameters.size)
         originalInitializationStatus = Array(parameters.size) { false }
