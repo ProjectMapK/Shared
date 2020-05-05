9a362d1d558b38b677860e40168f464ad040bd03
wrongwrong <boranti1995@gmail.com>
2020-05-06 06:07:12 +0900

    マップ用アダプタを追加

---
+++ src/main/kotlin/com/mapk/core/ArgumentAdaptor.kt
@@ -0,0 +1,21 @@
+package com.mapk.core
+
+import java.lang.IllegalArgumentException
+
+class ArgumentAdaptor(private val requiredParameters: Map<String, ValueParameter<*>>) {
+    private val argumentMap: MutableMap<String, Any?> = HashMap(requiredParameters.size, 2.0f)
+
+    fun isFullInitialized(): Boolean = requiredParameters.size == argumentMap.size
+
+    fun isInitialized(key: String): Boolean {
+        if (!requiredParameters.containsKey(key)) throw IllegalArgumentException("$key is not contains in parameters.")
+        return argumentMap.containsKey(key)
+    }
+
+    fun putIfAbsent(key: String, value: Any?) {
+        if (!isInitialized(key)) argumentMap[key] = value
+    }
+
+    // 事前に存在チェックはやるものと仮定してここでは読み出しだけ実装
+    internal fun readout(key: String): Any? = argumentMap[key]
+}
