2d0b97882719586b6ee5c077d3149559aa36155c
wrongwrong <boranti1995@gmail.com>
2020-05-06 06:09:49 +0900

    新方式に合わせバケツを修正

--- src/main/kotlin/com/mapk/core/ArgumentBucket.kt
+++ src/main/kotlin/com/mapk/core/ArgumentBucket.kt
@@ -3,21 +3,32 @@ package com.mapk.core
 import java.util.Objects
 import kotlin.reflect.KParameter
 
-class ArgumentBucket internal constructor(
+// TODO: 初期化の効率化方法の検討他
+internal class ArgumentBucket(
     private val keyList: List<KParameter>,
-    internal val valueArray: Array<Any?>,
-    private val isRequireNonNull: List<Boolean>,
-    initializationStatus: Array<Boolean>
+    val valueArray: Array<Any?>,
+    initializationStatus: Array<Boolean>,
+    argumentBinders: List<ArgumentBinder>,
+    adaptor: ArgumentAdaptor
 ) : Map<KParameter, Any?> {
-    private val initializationStatusManager = InitializationStatusManager(initializationStatus)
-    val isInitialized: Boolean get() = initializationStatusManager.isFullInitialized
-
     class Entry internal constructor(
         override val key: KParameter,
         override var value: Any?
     ) : Map.Entry<KParameter, Any?>
 
-    override val size: Int get() = initializationStatusManager.count
+    private val initializationStatusManager = InitializationStatusManager(initializationStatus)
+    val isInitialized: Boolean
+    override val size: Int
+
+    init {
+        argumentBinders.forEach {
+            val result = it.bindArgument(adaptor, valueArray)
+            if (result) initializationStatusManager.put(it.index)
+        }
+
+        isInitialized = initializationStatusManager.isFullInitialized
+        size = initializationStatusManager.count
+    }
 
     override fun containsKey(key: KParameter): Boolean {
         return initializationStatusManager.isInitialized(key.index)
@@ -38,17 +49,4 @@ class ArgumentBucket internal constructor(
         get() = keyList.filter { initializationStatusManager.isInitialized(it.index) }.toSet()
     override val values: Collection<Any?>
         get() = valueArray.filterIndexed { i, _ -> initializationStatusManager.isInitialized(i) }
-
-    fun putIfAbsent(key: KParameter, value: Any?) {
-        val index = key.index
-
-        // null入力禁止かつnullなら無視する
-        if (isRequireNonNull[index] && value == null) return
-
-        // 先に入ったものを優先するため、初期化済みなら何もしない
-        if (initializationStatusManager.isInitialized(index)) return
-
-        initializationStatusManager.put(index)
-        valueArray[index] = value
-    }
 }
