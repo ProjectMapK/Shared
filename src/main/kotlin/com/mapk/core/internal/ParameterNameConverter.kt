3cb8b593bb8b5e745bf7017786a162cda9f953a6
wrongwrong <boranti1995@gmail.com>
2020-05-06 17:35:38 +0900

    フォーマット修正

--- src/main/kotlin/com/mapk/core/internal/ParameterNameConverter.kt
+++ src/main/kotlin/com/mapk/core/internal/ParameterNameConverter.kt
@@ -9,8 +9,7 @@ internal sealed class ParameterNameConverter {
 
     class Simple(override val converter: (String) -> String) : ParameterNameConverter() {
         override fun convert(name: String) = converter(name)
-        override fun nest(infix: String, nameJoiner: NameJoiner) =
-            WithPrefix(infix, nameJoiner, converter)
+        override fun nest(infix: String, nameJoiner: NameJoiner) = WithPrefix(infix, nameJoiner, converter)
     }
 
     class WithPrefix(
@@ -22,11 +21,6 @@ internal sealed class ParameterNameConverter {
 
         // 結合を伴う変換では、「双方変換 -> 結合」の順で処理を行う
         override fun convert(name: String) = converter(name).let { nameJoiner.join(prefix, it) }
-        override fun nest(infix: String, nameJoiner: NameJoiner) =
-            WithPrefix(
-                convert(infix),
-                nameJoiner,
-                converter
-            )
+        override fun nest(infix: String, nameJoiner: NameJoiner) = WithPrefix(convert(infix), nameJoiner, converter)
     }
 }
