d78a0e22256ffb079905c105ec473ff105ff5174
wrongwrong <boranti1995@gmail.com>
2020-05-06 17:37:31 +0900

    固有名は埋め込む形に修正

--- src/main/kotlin/com/mapk/core/internal/Functions.kt
+++ src/main/kotlin/com/mapk/core/internal/Functions.kt
@@ -5,6 +5,7 @@ import com.mapk.annotations.KUseDefaultArgument
 import java.lang.IllegalArgumentException
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.findAnnotation
+import kotlin.reflect.jvm.jvmName
 
 /**
  * パラメータからエイリアスもしくはプロパティ名を取得する関数
@@ -16,7 +17,9 @@ internal fun KParameter.getAliasOrName(): String? = findAnnotation<KParameterAli
  */
 internal fun KParameter.isUseDefaultArgument(): Boolean {
     if (annotations.any { it is KUseDefaultArgument }) {
-        if (!isOptional) throw IllegalArgumentException("Find KUseDefaultArgument, but it's not has default argument.")
+        if (!isOptional) throw IllegalArgumentException(
+            "Find ${KUseDefaultArgument::class.jvmName}, but it's not has default argument."
+        )
         return true
     }
     return false
