d32c7445e12860ec007d646f7b77f97f40bd018f
wrongwrong <boranti1995@gmail.com>
2020-05-06 09:50:47 +0900

    ArgumentBinder.ValueにValueParameterを継承させる形に修正

--- src/main/kotlin/com/mapk/core/ArgumentBinder.kt
+++ src/main/kotlin/com/mapk/core/ArgumentBinder.kt
@@ -1,7 +1,11 @@
 package com.mapk.core
 
-internal sealed class ArgumentBinder {
+import com.mapk.annotations.KParameterRequireNonNull
+import kotlin.reflect.KClass
+
+internal sealed class ArgumentBinder(val annotations: List<Annotation>) {
     abstract val index: Int
+    val isNullable: Boolean = annotations.none { it is KParameterRequireNonNull }
 
     /**
      * 初期化されており、かつ読み出し条件に合致すれば読み出してその値をバインド、されていなければ何もしない
@@ -9,10 +13,16 @@ internal sealed class ArgumentBinder {
      */
     abstract fun bindArgument(adaptor: ArgumentAdaptor, valueArray: Array<Any?>): Boolean
 
-    class Value(private val key: String, override val index: Int) : ArgumentBinder() {
+    class Value<T : Any>(
+        override val index: Int,
+        annotations: List<Annotation>,
+        override val isOptional: Boolean,
+        override val name: String,
+        override val requiredClazz: KClass<T>
+    ) : ArgumentBinder(annotations), ValueParameter<T> {
         override fun bindArgument(adaptor: ArgumentAdaptor, valueArray: Array<Any?>): Boolean {
-            return if (adaptor.isInitialized(key)) {
-                valueArray[index] = adaptor.readout(key)
+            return if (adaptor.isInitialized(name)) {
+                valueArray[index] = adaptor.readout(name)
                 true
             } else {
                 false
@@ -22,8 +32,9 @@ internal sealed class ArgumentBinder {
 
     class Function(
         private val function: KFunctionForCall<*>,
-        override val index: Int
-    ) : ArgumentBinder() {
+        override val index: Int,
+        annotations: List<Annotation>
+    ) : ArgumentBinder(annotations) {
         override fun bindArgument(adaptor: ArgumentAdaptor, valueArray: Array<Any?>): Boolean {
             val temp = function.call(adaptor)
             valueArray[index] = temp
