739ee08850188f28b02eb804d33674eeb597f95c
wrongwrong <boranti1995@gmail.com>
2020-05-06 09:53:33 +0900

    ArgumentBinder.ValueにValueParameterを継承させる形に修正したためこちらは削除

--- src/main/kotlin/com/mapk/core/ValueParameter.kt
+++ src/main/kotlin/com/mapk/core/ValueParameter.kt
@@ -1,8 +1,6 @@
 package com.mapk.core
 
-import com.mapk.annotations.KParameterRequireNonNull
 import kotlin.reflect.KClass
-import kotlin.reflect.KParameter
 
 interface ValueParameter<T : Any> {
     val annotations: List<Annotation>
@@ -10,26 +8,4 @@ interface ValueParameter<T : Any> {
     val isOptional: Boolean
     val name: String
     val requiredClazz: KClass<T>
-}
-
-// TODO: 仮置き、ArgumentBinder.Valueでimplementした方が良いと感じるが一旦実装を分けている
-internal class ValueParameterImpl<T : Any>(
-    override val annotations: List<Annotation>,
-    override val isNullable: Boolean,
-    override val isOptional: Boolean,
-    override val name: String,
-    override val requiredClazz: KClass<T>
-) : ValueParameter<T> {
-    companion object {
-        fun newInstance(
-            parameter: KParameter,
-            parameterNameConverter: (String) -> String
-        ): ValueParameter<*> = ValueParameterImpl(
-            parameter.annotations,
-            parameter.annotations.any { it is KParameterRequireNonNull },
-            parameter.isOptional,
-            parameterNameConverter(parameter.getAliasOrName()!!),
-            parameter.type.classifier as KClass<*>
-        )
-    }
 }
