a68b238bd073a128fc39a4b86ec4d9bba5102d70
wrongwrong <boranti1995@gmail.com>
2020-08-08 15:13:30 +0900

    typo修正

--- src/main/kotlin/com/mapk/core/Functions.kt
+++ src/main/kotlin/com/mapk/core/Functions.kt
@@ -8,7 +8,7 @@ import kotlin.reflect.full.functions
 
 inline fun <reified A : Annotation> KClass<*>.getAnnotatedFunctionsFromCompanionObject(): Pair<Any, List<KFunction<*>>>? {
     return this.companionObject?.let { companionObject ->
-        val temp = companionObject.functions.filter { functions -> functions.annotations.any { it is A } }
+        val temp = companionObject.functions.filter { function -> function.annotations.any { it is A } }
 
         if (temp.isEmpty()) {
             // 空ならその後の処理をしてもしょうがないのでnullに合わせる
